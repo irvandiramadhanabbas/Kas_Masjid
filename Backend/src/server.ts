24e2e26ad7ef9fe849371b55c7e0fac28ca62287
M-SI <irvandiramadhana3@gmail.com>
2026-01-03 01:43:46 +0700

    update

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,7 +1,11 @@
 import app from "./app";
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT);
 
-app.listen(PORT, () => {
+if (!PORT) {
+  throw new Error("PORT is not defined");
+}
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
 });
