b86399acc8e74238f208b90d5947a7ec35626fa0
M-SI <irvandiramadhana3@gmail.com>
2026-01-03 01:29:22 +0700

    update

--- Backend/src/db.ts
+++ Backend/src/db.ts
@@ -4,9 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST ?? "localhost",
-  user: process.env.DB_USER ?? "root",
-  password: process.env.DB_PASS ?? "",
-  database: process.env.DB_NAME ?? "kas_masjid",
+  host: process.env.DB_HOST as string,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER as string,
+  password: process.env.DB_PASS as string,
+  database: process.env.DB_NAME as string,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
   dateStrings: true
 });
